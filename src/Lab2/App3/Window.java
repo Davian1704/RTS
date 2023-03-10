5ee1348f8a599163f8bfd8f053cad43d4dca0c77
Davian1704 <101044302+Davian1704@users.noreply.github.com>
2023-03-10 22:20:49 +0200

    lab2 app3

---
+++ src/Lab2/App3/Window.java
@@ -0,0 +1,37 @@
+package Lab2.App3;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Window extends JFrame implements Observer {
+
+    ArrayList<JPanel> squares=new ArrayList<JPanel>();
+
+    public Window() {
+        setLayout(null);
+        setSize(450,400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        init(3);
+        this.setVisible(true);
+    }
+    private void init(int n){
+        for(int i=0 ;i<n; i++){
+            JPanel square=new JPanel();
+            square.setLocation((i+1)*100,0);
+            square.setSize(50,50);
+            square.setBackground(Color.BLUE);
+            square.setVisible(true);
+            this.add(square);
+            this.squares.add(square);
+        }
+    }
+    @Override
+    public void update(Observable o, Object arg) {
+        squares.get(((Square)o).getId()).setLocation(((Square)o).getX(),((Square)o).getY());
+    }
+}
