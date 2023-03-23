f63e3dfd99e6eebe989b8dfd639f7a57c343c6be
Davian1704 <101044302+Davian1704@users.noreply.github.com>
2023-03-23 12:40:41 +0200

    Example 3

--- src/Lab3/Example3/Main.java
+++ src/Lab3/Example3/Main.java
@@ -3,11 +3,13 @@ package Lab3.Example3;
 class JoinTestThread extends Thread{
 
     Thread t;
+    int number;
+    static int result;
 
-    JoinTestThread(String n, Thread t){
+    JoinTestThread(String n, Thread t, int number){
 
         this.setName(n);
-
+        this.number=number;
         this.t=t;
 
     }
@@ -22,10 +24,15 @@ class JoinTestThread extends Thread{
 
             System.out.println("Thread "+this.getName()+" executing operation.");
 
+            for(int i=1;i<=number;i++){
+                if(number % i ==0)
+                    result+=i;
+            }
+
             Thread.sleep(3000);
 
             System.out.println("Thread "+this.getName()+" has terminated operation.");
-
+            if(t!=null) System.out.println(result);
         }
 
         catch(Exception e){e.printStackTrace();}
@@ -38,14 +45,14 @@ public class Main {
 
     public static void main(String[] args){
 
-        JoinTestThread w1 = new JoinTestThread("Thread 1",null);
+        JoinTestThread w1 = new JoinTestThread("Thread 1",null,2);
 
-        JoinTestThread w2 = new JoinTestThread("Thread 2",w1);
+        JoinTestThread w2 = new JoinTestThread("Thread 2",w1,4);
 
         w1.start();
 
         w2.start();
-
+        
     }
 
 }
